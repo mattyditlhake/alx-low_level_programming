baf1a628f99b15eb20ad5b157db158c36294401b
matty.ditlhake <matty.ditlhake@gmail.com>
2023-02-17 14:00:30 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("With proper grammar, but the outcomeis a piece of art,\n")
-		return (0);
+printf("With proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
